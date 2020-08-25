d75f97e1c41155758bd1ef6205d174dddc0da416
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-08-25 15:20:09 +0300

    string option

---
+++ code/confy/types/string.hpp
@@ -0,0 +1,94 @@
+// confy
+// Copyright (c) 2020 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#pragma once
+#include "generic_option.hpp"
+
+namespace confy
+{
+
+class string : public generic_option<std::string>
+{
+public:
+    string(std::string_view name, std::string_view abbr = {}, std::string_view desc = {})
+        : generic_option<std::string>(name, abbr, desc)
+    {
+        m_value_type = "string";
+    }
+
+    void add_enum_element(std::string_view elem)
+    {
+        m_enum.emplace_back(elem);
+    }
+
+    template <typename Option>
+    struct dsl_t : public generic_option<std::string>::dsl_t<Option>
+    {
+        using generic_option<std::string>::dsl_t<Option>::dsl_t;
+
+        auto& e(std::string_view name)
+        {
+            this->m_option.add_enum_element(name);
+            return this->self();
+        }
+    };
+    using dsl = dsl_t<string>;
+
+    virtual void write_value_type_desc(std::ostream& out) const override
+    {
+        if (!m_value_type_desc.empty())
+        {
+            out << m_value_type_desc;
+            return;
+        }
+
+        if (m_enum.empty())
+        {
+            write_value_type(out);
+            return;
+        }
+
+        bool first = true;
+        for (auto& e : m_enum)
+        {
+            if (first) first = false;
+            else out << '|';
+
+            out << e;
+        }
+    }
+
+    virtual bool do_set_from_string(std::string& val, std::string_view str)
+    {
+        if (m_enum.empty())
+        {
+            val = str;
+            return true;
+        }
+
+        for (const auto& e : m_enum)
+        {
+            if (e == str)
+            {
+                val = e;
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+private:
+    // the string type can double as an enum
+    // if the user adds a number of valid values
+    std::vector<std::string> m_enum;
+};
+
+template <>
+struct option_for_type<std::string> { using type = string; };
+
+}
--- scratch/scratch.cpp
+++ scratch/scratch.cpp
@@ -10,31 +10,9 @@
 #include <confy/confy.hpp>
 #include <confy/types/generic_option.hpp>
 #include <confy/types/simple_enum.hpp>
+#include <confy/types/string.hpp>
 
 using namespace std;
-
-class str : public confy::generic_option<std::string>
-{
-public:
-    str(std::string_view name = {}, std::string_view abbr = {}, std::string_view desc = {})
-        : confy::generic_option<std::string>(name, abbr, desc)
-    {
-        m_value_type = "string";
-        m_value_type_desc = "a text string";
-    }
-
-    virtual bool do_set_from_string(std::string& val, std::string_view str)
-    {
-        val = str;
-        return true;
-    }
-};
-
-namespace confy
-{
-template <>
-struct option_for_type<std::string> { using type = str; };
-}
 
 enum xxx
 {
@@ -62,7 +40,10 @@ int main()
                 .e(a, "a")
                 .e(b, "b")
                 .e(c, "c")
-            .cmd<str>("asd", "z")
+            .cmd<confy::string>("asd", "z")
+                .e("foo")
+                .e("bar")
+                .e("baz")
                 .desc("asdassgd fdzzZZZZZas sa")
                 .env("ZXXX")
             ;
