51d933a81f7851c8f96adcdfa5f09a7c46a41409
Borislav Stanimirov <b.stanimirov@abv.bg>
2020-08-25 13:24:10 +0300

    Basic option with values only

---
+++ code/confy/types/basic_option.hpp
@@ -0,0 +1,73 @@
+// confy
+// Copyright (c) 2020 Borislav Stanimirov
+//
+// Distributed under the MIT Software License
+// See accompanying file LICENSE.txt or copy at
+// https://opensource.org/licenses/MIT
+//
+#pragma once
+#include "../option.hpp"
+
+#include <optional>
+
+namespace confy
+{
+
+template <typename T>
+class basic_option : public option
+{
+public:
+    using option::option;
+
+    void set_value_ptr(T* ptr)
+    {
+        m_value_ptr = ptr;
+        if (ptr) m_default_value = *ptr;
+    }
+
+    template <typename Option>
+    struct dsl_t : public option::dsl_t<Option>
+    {
+        using option::dsl_t<Option>::dsl_t;
+        auto& val(T& v)
+        {
+            this->m_option.set_value_ptr(&v);
+            return this->self();
+        }
+    };
+    using dsl = dsl_t<basic_option<T>>;
+
+    virtual bool has_default_value() const final
+    {
+        return m_default_value.has_value();
+    }
+
+protected:
+    virtual bool validate(const T&) { return true; }
+
+    virtual bool set_from_default() final
+    {
+        if (m_default_value) return false;
+        if (!validate(*m_default_value)) return false;
+        if (!m_value_ptr) return true; // successful non-set :)
+        *m_value_ptr = *m_default_value;
+        return true;
+    }
+
+    virtual bool set_from_string(std::string_view str) final
+    {
+        T val;
+        if (!do_set_from_string(val, str)) return false;
+        if (!validate(val)) return false;
+        if (!m_value_ptr) return true;
+        *m_value_ptr = std::move(val);
+        return true;
+    }
+
+    virtual bool do_set_from_string(T& val, std::string_view str) = 0;
+
+    T* m_value_ptr = nullptr;
+    std::optional<T> m_default_value;
+};
+
+}
--- code/confy/types/generic_option.hpp
+++ code/confy/types/generic_option.hpp
@@ -5,7 +5,8 @@
 // See accompanying file LICENSE.txt or copy at
 // https://opensource.org/licenses/MIT
 //
-#include "../option.hpp"
+#pragma once
+#include "basic_option.hpp"
 
 #include <optional>
 #include <ostream>
@@ -13,10 +14,10 @@
 namespace confy
 {
 template <typename T>
-class generic_option : public option
+class generic_option : public basic_option<T>
 {
 public:
-    using option::option;
+    using basic_option<T>::basic_option;
 
     void set_value_ptr(T* ptr)
     {
@@ -25,14 +26,9 @@ public:
     }
 
     template <typename Option>
-    struct dsl_t : public option::dsl_t<Option>
+    struct dsl_t : public basic_option::dsl_t<Option>
     {
-        using option::dsl_t<Option>::dsl_t;
-        auto& val(T& v)
-        {
-            this->m_option.set_value_ptr(&v);
-            return this->self();
-        }
+        using basic_option::dsl_t<Option>::dsl_t;
     };
     using dsl = dsl_t<generic_option<T>>;
 
@@ -41,53 +37,19 @@ public:
         out << m_value_type;
     }
 
-    // print the value type description
     virtual void write_value_type_desc(std::ostream& out) const override
     {
         if (m_value_type_desc.empty()) write_value_type(out);
         else out << m_value_type_desc;
     }
 
-    // you can override this for options with no default value
-    virtual bool has_default_value() const final
-    {
-        return m_default_value.has_value();
-    }
-
-    // write the default value
     virtual void write_default_value(std::ostream& out) const final
     {
         if (m_default_value) out << *m_default_value;
     }
 
 protected:
-    virtual bool validate(const T&) { return true; }
-
     std::string m_value_type;
     std::string m_value_type_desc;
-
-    virtual bool set_from_default() final
-    {
-        if (m_default_value) return false;
-        if (!validate(*m_default_value)) return false;
-        if (!m_value_ptr) return true; // successful non-set :)
-        *m_value_ptr = *m_default_value;
-        return true;
-    }
-
-    virtual bool set_from_string(std::string_view str) final
-    {
-        T val;
-        if (!do_set_from_string(val, str)) return false;
-        if (!validate(val)) return false;
-        if (!m_value_ptr) return true;
-        *m_value_ptr = std::move(val);
-        return true;
-    }
-
-    virtual bool do_set_from_string(T& val, std::string_view str) = 0;
-
-    T* m_value_ptr = nullptr;
-    std::optional<T> m_default_value;
 };
 }
