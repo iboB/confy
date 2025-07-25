79f740d8cf79e9bd1d919e7523164ce47cf27e0c
Borislav Stanimirov <b@ibob.bg>
2025-07-25 07:25:56 +0300

    some test cleanup

---
+++ test/t-common_value_types.cpp
@@ -0,0 +1,7 @@
+// Copyright (c) Borislav Stanimirov
+// SPDX-License-Identifier: MIT
+//
+#include <confy/v/integer.hpp>
+#include <confy/v/std_string.hpp>
+#include <doctest/doctest.h>
+#include <confy/dict.hpp>
